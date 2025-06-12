d7ad44858fd9ba49cfc3e8ad3cd9fb6cbd99764c
Raju <raju.meenige@gmail.com>
2025-06-12 17:57:58 +0530

    Data Tramsformation done

---
+++ src/utils.py
@@ -0,0 +1,60 @@
+import sys
+import os
+from dataclasses import dataclass
+import dill
+# from sklearn.metrics import r2_score
+
+import numpy as np
+import pandas as pd
+# from sklearn.compose import ColumnTransformer # By using the pipe line transform  
+# from sklearn.impute import SimpleImputer
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+# from sklearn.model_selection import GridSearchCV
+
+
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        
+        os.makedirs(dir_path,exist_ok=True)
+        
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+'''
+def evaluate_models(x_train,y_train,x_test,y_test,models,params):
+    try:
+        report={}
+        
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            print(f"Evaluating model:  Type: {type(model)}")
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            #model.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            
+
+            y_train_pred=model.predict(x_train)
+            y_test_pred=model.predict(x_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+            
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    '''
