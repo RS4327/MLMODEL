189543dbfb016222d07914536a89a46666544a30
Raju <raju.meenige@gmail.com>
2025-06-13 11:02:13 +0530

    comiited

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 from dataclasses import dataclass
@@ -6,10 +7,10 @@ from sklearn.metrics import r2_score
 
 import numpy as np
 import pandas as pd
-# from sklearn.compose import ColumnTransformer # By using the pipe line transform  
-# from sklearn.impute import SimpleImputer
-# from sklearn.pipeline import Pipeline
-# from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.compose import ColumnTransformer # By using the pipe line transform  
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
@@ -55,6 +56,15 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,params):
             report[list(models.keys())[i]]=test_model_score
             
         return report
+
+
     except Exception as e:
         raise CustomException(e,sys)
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)    
